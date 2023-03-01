748be97cafbf4910c18feffc51b25eab855c1794
Melnikov Dmitriy <243orlr@mail.ru>
2023-03-01 17:44:53 +0700

    job mistake 1

--- Main.java
+++ Main.java
@@ -3,16 +3,19 @@ public class Main {
         Calculator calc = Calculator.instance.get();
         int a = calc.plus.apply(1, 2); // получаем а = 3
         int b = calc.minus.apply(1, 1); // получаем b = 0
-        int c = calc.devide.apply(a, b); // деление на ноль
-        calc.println.accept(c);
+        if(b == 0 ){
+            int c = calc.devide.apply(a, Integer.MAX_VALUE);
+            System.out.println("Деление на ноль !!!" );
+            calc.println.accept(c);
+        } else {
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        }
 
-        System.out.println(calc.abs.apply(-1));
+
+
+
 
 
     }
-}
-
-@FunctionalInterface
-interface Supplier {
-    Calculator get();
 }
