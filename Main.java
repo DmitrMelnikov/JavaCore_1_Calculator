1117f45ff27b5b8461fe0b4cd6d998069f55301c
Melnikov Dmitriy <243orlr@mail.ru>
2023-03-02 11:51:43 +0700

    job mistake 2

--- Main.java
+++ Main.java
@@ -1,21 +1,27 @@
+import java.util.function.BinaryOperator;
+
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
+
+        BinaryOperator<Integer> errorDevide = (x, y) -> {
+            if (y == 0) {
+                return Integer.MAX_VALUE;
+            } else {
+                return calc.devide.apply(x, y);
+            }
+        };
+
         int a = calc.plus.apply(1, 2); // получаем а = 3
         int b = calc.minus.apply(1, 1); // получаем b = 0
-        if(b == 0 ){
-            int c = calc.devide.apply(a, Integer.MAX_VALUE);
-            System.out.println("Деление на ноль !!!" );
+        int c = errorDevide.apply(a, b);
+
+        if (c == Integer.MAX_VALUE) {
+            System.out.println("Деление на ноль !!!");
             calc.println.accept(c);
         } else {
-            int c = calc.devide.apply(a, b);
             calc.println.accept(c);
         }
-
-
-
-
-
 
     }
 }
