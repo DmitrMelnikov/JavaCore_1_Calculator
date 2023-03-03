8e7ee587bf555a1d5baa7e4953a55ed97718c784
DmitrMelnikov <125539439+DmitrMelnikov@users.noreply.github.com>
2023-03-03 12:04:55 +0700

    Job mistake 4

--- Main.java
+++ Main.java
@@ -4,14 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
 
-        BinaryOperator<Integer> errorDevide = (x, y) -> {
-            if (y == 0) {
-                return Integer.MAX_VALUE;
-            } else {
-                return calc.devide.apply(x, y);
-            }
-        };
-
         int a = calc.plus.apply(1, 2); // получаем а = 3
         int b = calc.minus.apply(1, 1); // получаем b = 0
         int c = calc.devide.apply(a, b);
